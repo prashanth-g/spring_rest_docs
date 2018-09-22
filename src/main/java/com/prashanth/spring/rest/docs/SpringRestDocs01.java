917ee755d2f3238665313a0a943cfb8e07d8e510
prashanth-g <prashanthcsgce@gmail.com>
2018-09-22 20:45:52 +0530

    Added boot class

---
+++ src/main/java/com/prashanth/spring/rest/docs/SpringRestDocs01.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring.rest.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocs01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocs01.class, args);
+    }
+
+}
